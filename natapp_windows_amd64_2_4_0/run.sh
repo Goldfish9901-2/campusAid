8717fab2f648b15ddfbd3b7d83990c2de37b90ad
goldfish9901 <745727212@qq.com>
2025-05-13 22:11:27 +0800

    支付宝回调用内网穿透工具补全

--- natapp_windows_amd64_2_4_0/run.sh
+++ natapp_windows_amd64_2_4_0/run.sh
@@ -1,2 +1,5 @@
-./natapp -authtoken=$(cat ./auth.txt) 
+# 请在bash环境下（linux自带sh/bash /macOS自带zsh /windows git自带bash）环境下运行
+EXEC=./natapp
+[ "$OSTYPE" == "msys" ] && EXEC=./natapp.exe
+${EXEC} -authtoken=$(cat ./auth.txt) 
 # gradle bootRun
