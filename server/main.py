ad219d42a42a6326963404f91e95b7d52a793cd6
goldfish9901 <745727212@qq.com>
2025-05-18 00:24:42 +0800

    部署准备

--- server/main.py
+++ server/main.py
@@ -1,5 +1,13 @@
-from util import replace,getRootDir,getResourceDir
+from util import replace, getResourceDir
+
 if __name__ == '__main__':
-    print(getRootDir())
-    replace(getResourceDir()/"alipay.properties","http://m6626a.natappfree.cc","http://misaka52065.w1.luyouxia.net")
-    replace(getResourceDir()/"application.yml","8090","8000")
+    resource_dir = getResourceDir()
+
+    # 替换支付宝回调地址中的域名
+    replace(resource_dir / "alipay.properties", "http://m6626a.natappfree.cc", "http://misaka52065.w1.luyouxia.net")
+
+    # 直接替换 localhost:${server.port} → misaka52065.w1.luyouxia.net
+    replace(resource_dir / "alipay.properties", "localhost:\\\${server.port}", "misaka52065.w1.luyouxia.net")
+
+    # 替换 application.yml 中的端口（从 8090 → 8000）
+    replace(resource_dir / "application.yml", "8090", "8000")
