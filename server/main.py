a98c177bb4b37bb3bfd1d370c0b786e73dd63dc9
goldfish9901 <745727212@qq.com>
2025-05-17 20:59:21 +0800

    部署准备

--- server/main.py
+++ server/main.py
@@ -2,3 +2,4 @@ from util import replace,getRootDir,getResourceDir
 if __name__ == '__main__':
     print(getRootDir())
     replace(getResourceDir()/"alipay.properties","http://m6626a.natappfree.cc","http://misaka52065.w1.luyouxia.net")
+    replace(getResourceDir()/"application.yml","8090","8000")
