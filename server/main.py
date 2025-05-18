9c6386479470147eef98de2db21ffc8cf2a25e4b
goldfish9901 <745727212@qq.com>
2025-05-18 14:01:25 +0800

    部署用

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     replace(resource_dir / "alipay.properties", "http://m6626a.natappfree.cc", "http://misaka52065.w1.luyouxia.net")
 
     # 直接替换 localhost:${server.port} → misaka52065.w1.luyouxia.net
-    replace(resource_dir / "alipay.properties", "localhost:\\${server.port}", "misaka52065.w1.luyouxia.net")
+    replace(resource_dir / "alipay.properties", "localhost:${server.port}", "misaka52065.w1.luyouxia.net")
 
     # 替换 application.yml 中的端口（从 8090 → 8000）
     replace(resource_dir / "application.yml", "8090", "8000")
